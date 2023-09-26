5f32f5d46c680a87a9057a2b87cee9df629b7718
Lohit <00lohit@gmail.com>
2023-09-26 13:23:46 +0530

    fixu

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-
 import "./globals.css";
 
 export const metadata = {
@@ -12,6 +11,9 @@ import { Nav } from "@/components/Nav";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/fav.svg" />
+      </head>
       <body className={`${fonts.variable} font-sans`}>
         <Nav />
         {children}
